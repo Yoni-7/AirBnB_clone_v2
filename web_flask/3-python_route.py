cf025750d28a1e5941efd1a69dd931b13b88a8bc
Yoni-7 <yonatanassefa15@gmail.com>
2024-04-22 10:24:10 -0700

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,11 +3,8 @@
 starts a Flask web application
 """
 
-
 from flask import Flask
 app = Flask(__name__)
-
-
 
 
 @app.route('/', strict_slashes=False)
@@ -16,14 +13,10 @@ def index():
     return 'Hello HBNB!'
 
 
-
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
-
-
 
 
 @app.route('/c/<text>', strict_slashes=False)
@@ -32,14 +25,11 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
-
-
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
