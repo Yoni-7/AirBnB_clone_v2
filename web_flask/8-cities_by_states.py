a93c7c36acdf817e2fcdb4f624b09ead41e58745
Yoni-7 <yonatanassefa15@gmail.com>
2024-04-22 10:35:54 -0700

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,14 +2,10 @@
 """
 starts a Flask web application
 """
-
-
 from flask import Flask, render_template
 from models import *
 from models import storage
 app = Flask(__name__)
-
-
 
 
 @app.route('/cities_by_states', strict_slashes=False)
@@ -19,13 +15,10 @@ def cities_by_states():
     return render_template('8-cities_by_states.html', states=states)
 
 
-
-
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
