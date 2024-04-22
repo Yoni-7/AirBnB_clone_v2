8f7ffc0c1547e6288ea5edba98eff5b8bd3cfbf4
Yoni-7 <yonatanassefa15@gmail.com>
2024-04-22 10:34:15 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,14 +2,10 @@
 """
 starts a Flask web application
 """
-
-
 from flask import Flask, render_template
 from models import *
 from models import storage
 app = Flask(__name__)
-
-
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -19,13 +15,10 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
-
-
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
